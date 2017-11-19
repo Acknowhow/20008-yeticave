2f93c60693a09da7e318aa916d963cd4c1968435
Vadim Korabelnik <vad@oblako9.ru>
2017-11-19 22:59:37 +0300

    Some practice with headers

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ require 'data.php';
 $content = include_template('templates/index.php', [
   'categories' => $categories, 'items' => $items, 'lot_time_remaining' => $lot_time_remaining]);
 
+
 print include_template('templates/layout.php', [
   'content' => $content, 'title' => $title, 'is_auth' => $is_auth,
 
   'user_avatar' => $user_avatar, 'user_name' => $user_name, 'categories' => $categories, 'year_now' => $year_now
 ]);
+
